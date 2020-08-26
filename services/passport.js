c27a5316b17b3f9619bc2505040c73cc116c1b05
Matthew Wayne <matthewwayne@Matthews-MacBook-Pro.local>
2020-08-25 18:59:32 -0700

    WIP.

--- services/passport.js
+++ services/passport.js
@@ -14,12 +14,15 @@ passport.use( // passport.use makes aware that there is a new strategy available
       }, 
       (accessToken, refreshToken, profile, done) => {
         console.log('profile', profile);
-        User.findOne({ googleId: profile.id }).then(existingUser => {
+        User.findOne({ googleId: profile.id }).then(existingUser => { // anytime a query is made it is asynchronous and thus we receive a promise (think Cypress)
           if (existingUser) {
             // we already have a record with the given profile ID
+            done(null, existingUser); // we found the user and are done
           } else {
             // we don't have a user record with this ID, make a new record
-            new User({ googleId: profile.id }).save(); // creates a new instance of a user in our DB. must call "save" function to save it in the database.
+            new User({ googleId: profile.id }) // creates a new instance of a user in our DB.
+              .save() // must call "save" function to save it in the database. 
+              .then(user => done(null, user)); // saving is asynchrnous - do not call done without a then statement.
           }
         });
       }
