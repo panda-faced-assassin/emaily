353bdd303aa323f42b3a92846ef4f95e3c7c12f7
Matthew Wayne <matthewwayne@Matthews-MacBook-Pro.local>
2020-08-26 13:43:53 -0700

    WIP.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSeassion = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys');
 require('./models/User'); // must define the User model before using it in passport. order of require statements matter
 require('./services/passport');
@@ -7,6 +9,15 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSeassion({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys:  [keys.cookieKey] // make sure it as an error so that we can provide multiple keys for additional security
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes') (app);
 
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users'); // access to User model class. one argument means we're fetching something from model class, two args means loading something into class
 
+passport.serializeUser((user, done) => { // creates token for relogging
+  done(null, user.id); // user ID from Mongo db. Once a user signs in, we only care about our internal IDs.
+});
+
+passport.deserializeUser((id, done) => { // translates the token into the user.id
+  User.findById(id)
+    .then(user => { // remember, anytime you are interacting with MongoDB it is asynchronous and returns a promise
+      done(null, user)
+    }); 
+});
+
 passport.use( // passport.use makes aware that there is a new strategy available for auth use. new GoogleStrategy(pass in information on how to to configure authorization) creates new instance of Google strategy
     new GoogleStrategy( // google strategy has an internal identifier 'google' that can be used in the route handler
       {
@@ -22,7 +33,7 @@ passport.use( // passport.use makes aware that there is a new strategy available
             // we don't have a user record with this ID, make a new record
             new User({ googleId: profile.id }) // creates a new instance of a user in our DB.
               .save() // must call "save" function to save it in the database. 
-              .then(user => done(null, user)); // saving is asynchrnous - do not call done without a then statement.
+              .then(user => done(null, user)); // saving is asynchronous - do not call done without a then statement. null implies that you are not expecting errors (this is for simple processes)
           }
         });
       }
